19292c606d38896e2edb8aac2552256f9b75ee0b
HaZeL-spc <kacper.leszczu8@gmail.com>
2024-01-24 03:12:26 +0100

    change

---
+++ .ipynb_checkpoints/linear_distribution_images-checkpoint.py
@@ -0,0 +1,43 @@
+import os
+import random
+import shutil
+from collections import defaultdict
+import re
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+import numpy as np
+from PIL import Image
+
+def delete_photos_linear(folder):
+    source_images_dir = os.path.join(folder, 'UTKFace')
+    all_images = os.listdir(source_images_dir)
+    random.shuffle(all_images)
+
+    my_dict_ages = defaultdict(int)
+
+    for i, image in enumerate(all_images):
+        # print(image)
+        image_age = int(image.split("_")[0])//10
+        if (image_age >= 7):
+            image_age = 7
+        my_dict_ages[image_age] += 1
+
+    print(my_dict_ages)
+
+    min_photos = min(my_dict_ages.values())
+    my_dict_ages_new = defaultdict(int)
+
+    for i, image in enumerate(all_images):
+        image_age = int(image.split("_")[0])//10
+        if (image_age >= 7):
+            image_age = 7
+    
+        print(i)
+    
+        my_dict_ages_new[image_age] += 1
+        if (my_dict_ages_new[image_age] > min_photos):
+            image_file_name = os.path.join("UTKFace\\AugmentedUTKFace", image)
+            if os.path.isfile(image_file_name):
+                # Delete the file
+                os.remove(image_file_name)
+            else:
+                print("Error: %s file not found" % image)
---
+++ .ipynb_checkpoints/move_images_folders-checkpoint.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+import random
+
+def move_images(folder):
+    source_images_dir = os.path.join(folder, 'UTKFace')
+
+    train_images_dir = os.path.join(folder, 'Train')
+    test_images_dir = os.path.join(folder, 'Test')
+    val_images_dir = os.path.join(folder, 'Val')
+
+    os.makedirs(train_images_dir, exist_ok=True)
+    os.makedirs(test_images_dir, exist_ok=True)
+    os.makedirs(val_images_dir, exist_ok=True)
+    all_images = os.listdir(source_images_dir)
+    random.shuffle(all_images)
+
+    total_images = len(all_images)
+    train_split = int(0.7 * total_images)
+    test_split = int(0.15 * total_images)
+    val_split = total_images - train_split - test_split
+
+    for i, image in enumerate(all_images):
+        if i < train_split:
+            shutil.move(os.path.join(source_images_dir, image), os.path.join(train_images_dir, image))
+        elif i < train_split + test_split:
+            shutil.move(os.path.join(source_images_dir, image), os.path.join(test_images_dir, image))
+        else:
+            shutil.move(os.path.join(source_images_dir, image), os.path.join(val_images_dir, image))
